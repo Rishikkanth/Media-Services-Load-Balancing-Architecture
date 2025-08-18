66705facc2f00d667240abdcdc0566bc407d73e0
Rishikkanth <34005525+Rishikkanth@users.noreply.github.com>
2025-08-18 16:53:45 -0500

    Update variables.tf

--- IAC/Terraform/variables.tf
+++ IAC/Terraform/variables.tf
@@ -1,1 +1,8 @@
+variable "project"      { type = string }
+variable "vpc_id"       { type = string }
+variable "private_subnets" { type = list(string) }
+variable "public_subnets"  { type = list(string) }
+variable "certificate_arn" { type = string } # ACM cert for media.example.com
+variable "cluster_name" { type = string }
+variable "tg_healthy_path" { type = string default = "/status" }
 
