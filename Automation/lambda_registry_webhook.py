854cd6ba9d0e0aabd0d7f2c54bbacc5897a2485a
Rishikkanth <34005525+Rishikkanth@users.noreply.github.com>
2025-08-18 16:56:32 -0500

    Update lambda_registry_webhook.py

--- Automation/lambda_registry_webhook.py
+++ Automation/lambda_registry_webhook.py
@@ -1,1 +1,7 @@
+def register(node_id, path, tg_arn):
+  # look up rule for path, ensure weight to TG >= 1
+  elbv2.modify_rule(...set weight...)
+
+def deregister(node_id, path, tg_arn):
+  elbv2.modify_rule(...set weight 0 for that TG...)
 
